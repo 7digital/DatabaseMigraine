dfe80a791ddbfc838508feb79ca3b57d3ca1fdfe
Andres G. Aragoneses <andres@7digital.com>
2012-07-17 11:04:36 +0100

    Bisect.Tests: new Acceptance test for a sequence of ChooseTable operations
    WIP, doesn't pass yet

---
+++ src/DatabaseBisect.Tests/Acceptance/ClearAndTestSequence.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.SqlServer.Management.Smo;
+using NUnit.Framework;
+
+namespace DatabaseBisect.Tests.Acceptance
+{
+	[TestFixture]
+	public class ClearAndTestSequence : DbHelper
+	{
+		[Ignore("Not working yet, WIP")]
+		[Test]
+		public void ChooseTableASecondTimeDoesntChooseSameTableAgain ()
+		{
+			var db = GivenADisposableDbCreatedForTesting();
+			AndTheDbHasAtLeast2TablesAndSecondOneIsNotEmpty(db);
+			var firstTableBisected = AndIPerformTheClearAndTestOperationWithATestThatFails(db);
+			ThenTheTableChosenForSecondClearAndTestOperationIsNotTheSame(db, firstTableBisected);
+		}
+
+		private void ThenTheTableChosenForSecondClearAndTestOperationIsNotTheSame(Database db, Table firstTableBisected)
+		{
+			Assert.That(BisectOperations.ChooseTableToBisect(db), Is.Not.EqualTo(firstTableBisected));
+		}
+
+		private Table AndIPerformTheClearAndTestOperationWithATestThatFails(Database db)
+		{
+			var firstTableToBisect = BisectOperations.ChooseTableToBisect(db);
+			BisectOperations.BisectTableOnce(db, firstTableToBisect, () => false);
+			return firstTableToBisect;
+		}
+	}
+}
