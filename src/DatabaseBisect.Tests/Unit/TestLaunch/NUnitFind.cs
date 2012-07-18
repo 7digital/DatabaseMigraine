80d6de0562e01054e4ab87f3f84985e1e6fa9707
Andres G. Aragoneses <andres@7digital.com>
2012-07-18 21:27:32 +0100

    Bisect: implement the alternative findings for NUnitFinder

--- src/DatabaseBisect.Tests/Unit/TestLaunch/NUnitFind.cs
+++ src/DatabaseBisect.Tests/Unit/TestLaunch/NUnitFind.cs
@@ -13,8 +13,9 @@ namespace DatabaseBisect.Tests.Unit.TestLaunch
 		[Test]
 		public void NUnitIsLookedForInDefaultProgramFilesFolderByDefault ()
 		{
-			var nunitFinder = new NUnitFinder(null);
-			var firstPossibleLocation = nunitFinder.GetNUnitPossibleLocations().First();
+			var nunitFinder = new NUnitFinder(new FakeEverythingExists());
+			var firstPossibleLocation = nunitFinder.GetNUnitPossibleLocations().FirstOrDefault();
+			Assert.That(firstPossibleLocation, Is.Not.Null);
 			Assert.That(firstPossibleLocation, Is.EqualTo(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)));
 			Assert.That(Directory.Exists(firstPossibleLocation));
 		}
@@ -33,7 +34,48 @@ namespace DatabaseBisect.Tests.Unit.TestLaunch
 		{
 			var nunitFinder = new NUnitFinder(new FakeNothingExists());
 			var nunitPossibleLocations = nunitFinder.GetNUnitPossibleLocations();
-			Assert.That(nunitPossibleLocations.Count(), Is.EqualTo(1));
+			Assert.That(nunitPossibleLocations.Count(), Is.EqualTo(0));
+		}
+
+
+		class FakeEverythingExists : IDirectory
+		{
+			public bool Exists(string path)
+			{
+				return true;
+			}
+		}
+
+		[Test]
+		public void PossibleNUnitLocationsAfterDefaultAreAllKnownArchs()
+		{
+			var nunitFinder = new NUnitFinder(new FakeEverythingExists());
+			var nunitPossibleLocations = new List<string> (nunitFinder.GetNUnitPossibleLocations());
+			Assert.That(nunitPossibleLocations.Count(), Is.GreaterThan(1));
+			var nunitPossibleLocationsAfterDefault = RemoveFirstElement(nunitPossibleLocations);
+
+			var possibleArchs = new List<string> { "x86", "x64" };
+			Assert.That(nunitPossibleLocationsAfterDefault.Count(), Is.EqualTo(possibleArchs.Count));
+			var expectedPossibleLocationsAfterDefault = new List<string>();
+			var defaultProgramFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+			foreach(var arch in possibleArchs)
+			{
+				expectedPossibleLocationsAfterDefault.Add(String.Format("{0} ({1})", defaultProgramFiles, arch));
+			}
+
+			for(int i = 0; i < possibleArchs.Count; i++)
+			{
+				var expected = expectedPossibleLocationsAfterDefault[i];
+				Assert.That(nunitPossibleLocationsAfterDefault.ElementAt(i), Is.EqualTo(expected),
+				            String.Format("Incorrect possible nunit location at iteration number {0}", i + 1));
+			}
+		}
+
+		private List<string> RemoveFirstElement(List<string> nunitPossibleLocations)
+		{
+			var newList = new List<string>(nunitPossibleLocations);
+			newList.Remove(nunitPossibleLocations[0]);
+			return newList;
 		}
 	}
 
@@ -44,13 +86,26 @@ namespace DatabaseBisect.Tests.Unit.TestLaunch
 
 	public class NUnitFinder
 	{
+		private readonly IDirectory _directoryService;
+
 		public NUnitFinder (IDirectory directoryService)
 		{
-			
+			_directoryService = directoryService;
 		}
+
 		public IEnumerable<string> GetNUnitPossibleLocations()
 		{
-			yield return Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+			var defaultProgramFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+			if (_directoryService.Exists(defaultProgramFiles))
+				yield return defaultProgramFiles;
+
+			var alternative = defaultProgramFiles + " (x86)";
+			if (_directoryService.Exists(alternative))
+				yield return alternative;
+
+			alternative = defaultProgramFiles + " (x64)";
+			if (_directoryService.Exists(alternative))
+				yield return alternative;
 		}
 	}
 }
