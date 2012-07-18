afd09ded4db6941a50b60c1c3d9c549957a0ef41
Andres G. Aragoneses <andres@7digital.com>
2012-07-18 20:52:17 +0100

    Bisect: 2nd basic test for NUnitFinder
    All paths returned by NUnitFinder should exist, so if the
    DirectoryService returns false for all of them, we only have
    one element in the possibleNUnitLocations sequence.

--- src/DatabaseBisect.Tests/Unit/TestLaunch/NUnitFind.cs
+++ src/DatabaseBisect.Tests/Unit/TestLaunch/NUnitFind.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using NUnit.Framework;
 
@@ -12,17 +13,42 @@ namespace DatabaseBisect.Tests.Unit.TestLaunch
 		[Test]
 		public void NUnitIsLookedForInDefaultProgramFilesFolderByDefault ()
 		{
-			var nunitFinder = new NUnitFinder();
-			Assert.That(nunitFinder.GetNUnitPossibleLocation().First(),
-				Is.EqualTo(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)));
+			var nunitFinder = new NUnitFinder(null);
+			var firstPossibleLocation = nunitFinder.GetNUnitPossibleLocations().First();
+			Assert.That(firstPossibleLocation, Is.EqualTo(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)));
+			Assert.That(Directory.Exists(firstPossibleLocation));
+		}
+
+		class FakeNothingExists : IDirectory
+		{
+			public bool Exists(string path)
+			{
+				return false;
+			}
 		}
 
 
+		[Test]
+		public void NUnitIsLookedForInThreePossibleProgramFiles()
+		{
+			var nunitFinder = new NUnitFinder(new FakeNothingExists());
+			var nunitPossibleLocations = nunitFinder.GetNUnitPossibleLocations();
+			Assert.That(nunitPossibleLocations.Count(), Is.EqualTo(1));
+		}
+	}
+
+	public interface IDirectory
+	{
+		bool Exists(string path);
 	}
 
 	public class NUnitFinder
 	{
-		public IEnumerable<string> GetNUnitPossibleLocation()
+		public NUnitFinder (IDirectory directoryService)
+		{
+			
+		}
+		public IEnumerable<string> GetNUnitPossibleLocations()
 		{
 			yield return Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
 		}
