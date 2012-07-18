1d6bb40c5a2837b9be8183f82723e883ba0b53ad
Andres G. Aragoneses <andres@7digital.com>
2012-07-18 20:30:23 +0100

    Bisect: first very simple test for NUnitFinder

---
+++ src/DatabaseBisect.Tests/Unit/TestLaunch/NUnitFind.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace DatabaseBisect.Tests.Unit.TestLaunch
+{
+	[TestFixture]
+	public class NUnitFind
+	{
+		[Test]
+		public void NUnitIsLookedForInDefaultProgramFilesFolderByDefault ()
+		{
+			var nunitFinder = new NUnitFinder();
+			Assert.That(nunitFinder.GetNUnitPossibleLocation().First(),
+				Is.EqualTo(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)));
+		}
+
+
+	}
+
+	public class NUnitFinder
+	{
+		public IEnumerable<string> GetNUnitPossibleLocation()
+		{
+			yield return Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+		}
+	}
+}
