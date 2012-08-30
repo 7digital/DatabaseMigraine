339a398139f18097d56dc925ef7a682302fe5275
Antony Denyer <antonydenyer@gmail.com>
2012-08-30 14:46:08 +0100

    add expcetion handling to exit the program with error code

--- src/DatabaseCreator/Program.cs
+++ src/DatabaseCreator/Program.cs
@@ -14,44 +14,54 @@ namespace DatabaseCreator
 
 		static void Main(string[] args)
 		{
-			if(args.Length < 1 || args.Length > 2 || (args.Length == 2 && !args[1].Contains("--config:")))
+			try
 			{
-				Console.WriteLine("Please supply the name of database(s) you wish to create.");
-				Console.WriteLine();
-				Console.WriteLine("Optionally in the 2nd argument, specify a config file to sabotage with:");
-				Console.WriteLine("--config:\"/path/to/first.config.file{0}/path/to/second.config.file\"",
-				                  Path.PathSeparator);
-				Console.WriteLine("To Exit Press Any Key");
-				Console.Read();
-				Environment.Exit(0);
-			}
 
-			string[] configFiles = null;
-			if (args.Length > 1)
-			{
-				string paths = args[1].Substring(args[1].IndexOf(":") + 1);
-
-				configFiles = paths.Split(Path.PathSeparator);
-				foreach(var configFile in configFiles)
+				if (args.Length < 1 || args.Length > 2 || (args.Length == 2 && !args[1].Contains("--config:")))
 				{
-					Console.WriteLine(configFile);
-					if (!File.Exists(configFile))
+					Console.WriteLine("Please supply the name of database(s) you wish to create.");
+					Console.WriteLine();
+					Console.WriteLine("Optionally in the 2nd argument, specify a config file to sabotage with:");
+					Console.WriteLine("--config:\"/path/to/first.config.file{0}/path/to/second.config.file\"",
+									  Path.PathSeparator);
+					Console.WriteLine("To Exit Press Any Key");
+					Console.Read();
+					Environment.Exit(0);
+				}
+
+				string[] configFiles = null;
+				if (args.Length > 1)
+				{
+					string paths = args[1].Substring(args[1].IndexOf(":") + 1);
+
+					configFiles = paths.Split(Path.PathSeparator);
+					foreach (var configFile in configFiles)
 					{
-						throw new FileNotFoundException(configFile);
+						Console.WriteLine(configFile);
+						if (!File.Exists(configFile))
+						{
+							throw new FileNotFoundException(configFile);
+						}
 					}
 				}
-			}
 
-			string disposableDbName = CreateDatabase(args[0]);
+				string disposableDbName = CreateDatabase(args[0]);
 
-			if (configFiles != null)
-			{
-				foreach(var configFile in configFiles)
+				if (configFiles != null)
 				{
-					ConfigFileSaboteur.Sabotage(configFile, args[0], disposableDbName);
+					foreach (var configFile in configFiles)
+					{
+						ConfigFileSaboteur.Sabotage(configFile, args[0], disposableDbName);
+					}
 				}
+				Environment.Exit(0);
+
 			}
-			Environment.Exit(0);
+			catch (Exception exception)
+			{
+				Console.WriteLine(exception.Message);
+				Environment.Exit(1);
+			}
 		}
 
 		private static string CreateDatabase(string dbNameInVcs)
